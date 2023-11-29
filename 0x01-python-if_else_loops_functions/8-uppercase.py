a8bba2a88a3578ce6410bef8b7ca71878ae4091d
coxy-code <cosmaskimutai.ck@gmail.com>
2023-11-29 06:14:50 -0800

    0x01-python-if_else_loop_functions

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        if ord('a') <= ord(char) <= ord('z'):
+            print(chr(ord(char) - 32), end='')
+        else:
+            print(char, end='')
+    print()
