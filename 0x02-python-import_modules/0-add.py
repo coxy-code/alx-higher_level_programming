5081f4034db34f72d4ca513319afbb0f5a67e2d5
coxy-code <cosmaskimutai.ck@gmail.com>
2023-11-30 01:31:01 -0800

    function def add(a, b)

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 a = 1
 b = 2
-add = __import__("add_0").add
-print("{} + {} = {}".format(a, b, add(a, b)))
-
+add_0 = __import__('add_0').add
+print("{} + {} = {}".format(a, b, add_0(a, b)))
