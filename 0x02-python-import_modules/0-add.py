c1ea5ac40ffb11664af46a913727890686816fe2
coxy-code <cosmaskimutai.ck@gmail.com>
2023-11-30 01:50:22 -0800

    function def add(a, b)

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-# Importing add function from add_0.py
-from add_0 import add
-
-# Using string formatting to print the result
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == '__main__':
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
