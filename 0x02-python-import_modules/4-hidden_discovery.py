d48e8991d1f00352fea90efc4aae48806eac05d9
coxy-code <cosmaskimutai.ck@gmail.com>
2023-12-04 22:42:44 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv[1:]: 
-        result += int(arg)
-    print(result)
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
