95ffd00fbbf7e89248b9ca6386a1aa0add938c2e
coxy-code <cosmaskimutai.ck@gmail.com>
2023-12-04 22:38:55 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    result = 0
-    for arg in sys.argv[1:]:  # Start from the second element to skip the script name
-        result += int(arg)
-    print(result)
+import sys
+result = 0
+for arg in sys.argv[1:]:
+result += int(arg)
+print(result)
