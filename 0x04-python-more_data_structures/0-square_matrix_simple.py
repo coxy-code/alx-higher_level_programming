0ddd2f859f1eae0082714d3d1f5a82539d6720bb
coxy-code <cosmaskimutai.ck@gmail.com>
2023-12-06 02:14:00 -0800

    0x04-python-more_data_structure

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,10 +3,7 @@
 def square_matrix_simple(matrix=[]):
     # Create a new matrix to store squared values (deep copy)
     new_matrix = [row[:] for row in matrix]
-    
-    # Iterate through each row and column to square the values
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             new_matrix[i][j] = matrix[i][j] ** 2  # Square the value
-            
-    return new_matrix
+        return new_matrix
