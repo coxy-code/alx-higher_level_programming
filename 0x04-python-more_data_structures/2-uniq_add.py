c31ad2ea8dbffcdf008e3a1fb4a8aa69a7127ac8
coxy-code <cosmaskimutai.ck@gmail.com>
2023-12-06 02:00:18 -0800

    0x04-python-more_data_structure

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return(sum(set(my_list)))
+    unique_set = set()
+    sum_unique = 0
+    
+    for num in my_list:
+        if num not in unique_set:  # Check if the number is not already in the set
+            sum_unique += num     # Add the unique number to the sum
+            unique_set.add(num)   # Add the unique number to the set
+    
+    return sum_unique
