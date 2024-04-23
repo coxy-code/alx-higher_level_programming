cd639ac6fdd5a63085cc4f7b66c3aebfeedb18dc
coxy-code <cosmaskimutai.ck@gmail.com>
2024-04-23 04:07:20 -0700

    web_scrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-// Read from file
+const fs = require('fs');
 
-const filesys = require('fs');
-filesys.readFile(process.argv[2], 'utf-8',
-  function (err, data) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(data);
-  });
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  process.stdout.write(data);
+});
